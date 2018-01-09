89055052b0f6dd2fa2b82020bc522a16019ed56c
lilincpp <liincpp@gmail.com>
2018-01-09 22:50:41 +0800

    增加两个示例代码

--- TicketDemo/app/src/main/java/com/test/ticketdemo/MainActivity.java
+++ TicketDemo/app/src/main/java/com/test/ticketdemo/MainActivity.java
@@ -2,7 +2,12 @@ package com.test.ticketdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TableRow;
+
+import com.lilincpp.ticketview.TicketParam;
+import com.lilincpp.ticketview.support.drawable.SimpleTicketDrawable;
+import com.lilincpp.ticketview.support.shape.SimpleRoundShape;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        LinearLayout two = findViewById(R.id.ll_two);
+
+        SimpleTicketDrawable.Builder builder =
+                new SimpleTicketDrawable.Builder(two.getBackground());
+        SimpleRoundShape leftAndRight = new SimpleRoundShape();
+        SimpleRoundShape bottomAndTop = new SimpleRoundShape();
+        bottomAndTop.setRadius(leftAndRight.getRadius() * 4);
+        bottomAndTop.setQuantity(1);
+        bottomAndTop.setStartDrawGravity(TicketParam.DrawGravity.CENTER);
+        builder.setBoundaryShape(
+                leftAndRight,
+                bottomAndTop,
+                leftAndRight,
+                bottomAndTop
+        );
+        two.setBackground(builder.create());
     }
 }
